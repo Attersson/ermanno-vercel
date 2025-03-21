d2430e7afeaba6368ec5510d0d7b2d1cb7a1e5ce
Ermanno Attardo <ermanno@attardo.it>
2025-03-21 18:53:57 +0100

    Add testing

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1', // Adjust for Next.js absolute imports
+  },
+};
